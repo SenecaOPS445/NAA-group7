7febeb4bd20d5398eefde9ea6325337066d223b0
Marian <mdfernando3@myseneca.ca>
2024-11-19 07:22:33 +0000

    disk usage task completed

--- assignment2.py
+++ assignment2.py
@@ -1,2 +1,13 @@
+def check_disk_usage(disk):
+    "Check the usage of the disk"
+    try:
+        print(f"Checking disk usage for {disk}...")
+        usage = shutil.disk_usage(disk)
+        print(f"Total: {usage.total // (1024 ** 3)} GB")
+        print(f"Used: {usage.used // (1024 ** 3)} GB")
+        print(f"Free: {usage.free // (1024 ** 3)} GB\n")
+    except Exception as e:
+        print(f"Error checking for disk usage: {e}")
+            
 
 
